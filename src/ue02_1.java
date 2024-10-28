08d210e4dd309e3d8b2eff3009fff96fb833450b
s2410238083 <s2410238083@fhooe.at>
2024-10-28 17:06:27 +0100

    Übung2_first commit

--- src/ue02_1.java
+++ src/ue02_1.java
@@ -1,7 +1,34 @@
+import libraries.In;
 import libraries.Out;
 
 public class ue02_1 {
     public static void main(String[] args) {
         Out.println("Hello, World!");
+
+        Out.println("Enter first number");
+        int FirstNumber = In.readInt();
+
+        Out.println("Enter second number");
+        int SceondNumber = In.readInt();
+
+        Out.println("Enter third number");
+        int ThirdNumber = In.readInt();
+
+        int max = 0;
+
+        if (FirstNumber >= SceondNumber && FirstNumber >= ThirdNumber) {
+            max = FirstNumber;
+          }
+           
+          else if (SceondNumber >= FirstNumber && SceondNumber >= ThirdNumber) {
+            max = SceondNumber;
+          }
+
+           else if (ThirdNumber >= FirstNumber && ThirdNumber >= SceondNumber) {
+            max = ThirdNumber;
+           }
+            
+           Out.println("Maximun");
+           Out.print (max);
+        }
     }
-}
