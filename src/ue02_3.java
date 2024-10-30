f0b68bf6e99260d1e31e15e78cd883a130d5db4c
s2410238083 <s2410238083@fhooe.at>
2024-10-30 10:42:58 +0100

    commit

--- src/ue02_3.java
+++ src/ue02_3.java
@@ -1,7 +1,21 @@
+import libraries.In;
 import libraries.Out;
 
 public class ue02_3 {
     public static void main(String[] args) {
         Out.println("Hello, World!");
+
+        Out.println("Enter a x-Koordinate");
+        double x = In.readDouble();
+
+        Out.println("Enter a y-Koordinate");
+        double y = In.readDouble();
+
+        double radius = 4;
+        double Mittelpunktx = 0;
+        double Mittelpunkty = 0;
+
+        if ((x <= 4 && x >= 0) && (y <= 4 && y >= 0)) {
+        }
     }
 }
