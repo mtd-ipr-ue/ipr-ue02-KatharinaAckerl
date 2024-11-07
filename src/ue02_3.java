4f5d9b2efe81b24612906ba514c219cc6401c8fa
s2410238083 <s2410238083@fhooe.at>
2024-11-07 12:11:15 +0100

    ue2_3.java

--- src/ue02_3.java
+++ src/ue02_3.java
@@ -5,17 +5,27 @@ public class ue02_3 {
     public static void main(String[] args) {
         Out.println("Hello, World!");
 
-        Out.println("Enter a x-Koordinate");
+        Out.println("Enter a x-cordinate");
         double x = In.readDouble();
 
-        Out.println("Enter a y-Koordinate");
+        Out.println("Enter a y-cordinate");
         double y = In.readDouble();
 
         double radius = 4;
-        double Mittelpunktx = 0;
-        double Mittelpunkty = 0;
+        double centerx = 0;
+        double centery = 0;
+        double Circle = (Math.PI * radius * radius)/4;
 
         if ((x <= 4 && x >= 0) && (y <= 4 && y >= 0)) {
+            if (x*y > Circle) {
+                Out.println("The cordinates are inside the green area");
+                }
+            else {
+                Out.println("The cordinates are not inside the green area");
+            }
+        } 
+        else {
+            Out.println("The cordinates are not inside the green area");
         }
     }
 }
